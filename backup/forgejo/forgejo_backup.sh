34436b3eb7d9a936cd40bf840910a79a8873d044
Leaaep <github.scurvy309@passmail.net>
2025-09-05 22:22:03 +0200

    fix: backup scripts

--- backup/forgejo/forgejo_backup.sh
+++ backup/forgejo/forgejo_backup.sh
@@ -2,26 +2,26 @@
 set -euo pipefail
 
 # Load Forgejo env
-if [ ! -f .env.forgejo ]; then
-    echo ".env.forgejo not found!"
+if [ ! -f .env ]; then
+    echo ".env not found!"
     exit 1
 fi
-source .env.forgejo
+source .env
 
 # Prepare backup directory
 BACKUP_DIR="$BACKUP_TMP_DIR/forgejo-backup-$(date +%Y-%m-%d)"
 mkdir -p "$BACKUP_DIR"
-
-echo "Stopping Forgejo container..."
-docker stop "$CONTAINER_FORGEJO"
 
 echo "Running Forgejo dump..."
 DUMP_FILE="dump-$(date +%Y-%m-%d).tar"
 docker exec "$CONTAINER_FORGEJO" forgejo dump "$FORGEJO_DUMP_PATH/$DUMP_FILE"
 docker cp "$CONTAINER_FORGEJO:$FORGEJO_DUMP_PATH/$DUMP_FILE" "$BACKUP_DIR/forgejo-dump.tar"
 
+echo "Stopping Forgejo container..."
+docker stop "$CONTAINER_FORGEJO"
+
 echo "Copying SQLite database..."
-docker cp "$CONTAINER_FORGEJO:$FORGEJO_SQLITE_PATH" "$BACKUP_DIR/forgejo.sqlite"
+cp "$LOCAL_SQLITE_PATH" "$BACKUP_DIR/forgejo.sqlite"
 
 echo "Restarting Forgejo container..."
 docker start "$CONTAINER_FORGEJO"
--- backup/mealie/mealie_backup.sh
+++ backup/mealie/mealie_backup.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 # Load Mealie env
-if [ ! -f .env.mealie ]; then
-    echo ".env.mealie not found!"
+if [ ! -f .env ]; then
+    echo ".env not found!"
     exit 1
 fi
-source .env.mealie
+source .env
 
 # Prepare backup directory
 BACKUP_DIR="$BACKUP_TMP_DIR/mealie-backup-$(date +%Y-%m-%d)"
@@ -16,7 +16,7 @@ echo "Stopping Mealie container..."
 docker stop "$CONTAINER_MEALIE"
 
 echo "Backing up Mealie data..."
-docker cp "$CONTAINER_MEALIE:$MEALIE_DATA_PATH" "$BACKUP_DIR/"
+cp "$MEALIE_DATA_PATH" "$BACKUP_DIR/"
 
 echo "Restarting Mealie container..."
 docker start "$CONTAINER_MEALIE"
